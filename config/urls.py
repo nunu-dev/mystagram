b92ef1138825745c54fb6b7d1c1dd8da65e7d635
eunwoo <aufcl4858@naver.com>
2018-08-18 16:50:42 +0900

    Getting my notifications

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,10 @@ urlpatterns = [
         "images/",
         include("mystagram.images.urls", namespace="images"),
     ),
+    path(
+        "notifications/",
+        include("mystagram.notifications.urls", namespace="notifications"),
+    ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(
