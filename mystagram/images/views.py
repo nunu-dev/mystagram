980813416f6382fd66b27ee9ec9860daf8ebcf77
eunwoo <aufcl4858@naver.com>
2018-08-18 22:36:17 +0900

    Creating Follow, Comment and Like notification

--- mystagram/images/views.py
+++ mystagram/images/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import models, serializers
+from mystagram.notifications import views as notification_views
 
 class Feed(APIView):
 
@@ -51,8 +52,11 @@ class LikeImage(APIView):
                 creator = user,
                 image = found_image
             )
-
+            
             new_like.save()
+
+            notification_views.create_norification(user, found_image.creator,'like', found_image)
+
             return Response(status=status.HTTP_201_CREATED)
 
 class UnLikeImage(APIView):
@@ -95,7 +99,9 @@ class CommentOnImage(APIView):
 
         if serializer.is_valid():
 
-            serializer.save(creator=user)
+            serializer.save(creator=user, image=found_image)
+
+            notification_views.create_norification(user, found_image.creator, 'comment', found_image, serializer.data['message'])
 
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
--- mystagram/users/views.py
+++ mystagram/users/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import models, serializers
+from mystagram.notifications import views as notification_views
 
 class ExploreUsers(APIView):
 
@@ -27,6 +28,8 @@ class FollowUser(APIView):
         user.following.add(user_to_follow)
 
         user.save()
+
+        notification_views.create_norification(user, user_to_follow, 'follow')
 
         return Response(status=status.HTTP_200_OK)
 
