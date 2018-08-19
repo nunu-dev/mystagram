5af3614f3817ddee9e67ccb610d9e64d0a10dcaf
eunwoo <aufcl4858@naver.com>
2018-08-19 19:03:59 +0900

    Getting the image likes

--- mystagram/images/views.py
+++ mystagram/images/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from mystagram.users import models as user_models
 from . import models, serializers
+from mystagram.users import serializers as user_serializers
 from mystagram.notifications import views as notification_views
 
 class Feed(APIView):
@@ -35,6 +37,18 @@ class Feed(APIView):
         return Response(serializer.data)
 
 class LikeImage(APIView):
+
+    def get(self, request, image_id, format=None):
+
+        likes = models.Like.objects.filter(image__id=image_id)
+
+        like_creators_ids = likes.values('creator_id')
+
+        users = user_models.User.objects.filter(id__in=like_creators_ids)
+
+        serializer = user_serializers.ListUserSerializer(users, many=True)
+
+        return Response(data=serializer.data, status= status.HTTP_200_OK)
 
     def post(self, request, image_id, format=None):
 
