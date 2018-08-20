ea8fb6b33a28590cb824c966487cab8fffcb1bac
eunwoo <aufcl4858@naver.com>
2018-08-20 22:47:26 +0900

    Setting up JWT

--- config/urls.py
+++ config/urls.py
@@ -4,17 +4,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/",
-        TemplateView.as_view(template_name="pages/about.html"),
-        name="about",
-    ),
+    
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
+    path("api-token-auth/", obtain_jwt_token),
     path(
         "users/",
         include("mystagram.users.urls", namespace="users"),
