90cf281a901425cfea2f051691e390686c9bd417
eunwoo <aufcl4858@naver.com>
2018-09-30 13:22:36 +0900

    Auth Component part One

---
+++ frontend/src/components/Auth/index.js
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux';
+import Container from './container';
+
+// Add all the actions for:
+// Log in
+// sign up
+// Recover password
+// Check username
+// Check password
+
+export default connect()(Container);
