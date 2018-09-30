5ebc23ed34d289f4598e2ede58abaa41dc5879d0
eunwoo <aufcl4858@naver.com>
2018-09-30 14:30:51 +0900

    Finishing Auth Component and the Auth Forms

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,35 +1,32 @@
-import React from 'react';
-import styles from './styles.scss';
-import { LoginForm, SignupForm } from 'components/AuthForms';
+import React from "react";
+import styles from "./styles.scss";
+import { LoginForm, SignupForm } from "components/AuthForms";
 
 const Auth = (props, context) => (
   <main className={styles.auth}>
     <div className={styles.column}>
-      <img
-        src={require('../../images/phone.png')}
-        alt="Checkout our app. Is cool"
-      />
+      <img src={require("images/phone.png")} alt="Checkout our app. Is cool" />
     </div>
     <div className={styles.column}>
       <div className={`${styles.whiteBox} ${styles.formBox}`}>
-        <img src={require('images/logo.png')} alt="Logo" />
-        {props.action === 'login' && <LoginForm />}
-        {props.action === 'signup' && <SignupForm />}
+        <img src={require("images/logo.png")} alt="Logo" />
+        {props.action === "login" && <LoginForm />}
+        {props.action === "signup" && <SignupForm />}
       </div>
       <div className={styles.whiteBox}>
-        {props.action === 'login' && (
-          <p>
-            Don't have an account?{' '}
+        {props.action === "signup" && (
+          <p className={styles.text}>
+            Have an account?{" "}
             <span className={styles.changeLink} onClick={props.changeAction}>
-              Sign up
+              Log in
             </span>
           </p>
         )}
-        {props.action === 'signup' && (
-          <p>
-            Have an account?{' '}
+        {props.action === "login" && (
+          <p className={styles.text}>
+            Don't have an account?{" "}
             <span className={styles.changeLink} onClick={props.changeAction}>
-              Log in
+              Sign up
             </span>
           </p>
         )}
@@ -38,11 +35,11 @@ const Auth = (props, context) => (
         <span>Get the app</span>
         <div className={styles.appstores}>
           <img
-            src={require('images/ios.png')}
+            src={require("images/ios.png")}
             alt="Download it on the Apple Appstore"
           />
           <img
-            src={require('images/android.png')}
+            src={require("images/android.png")}
             alt="Download it on the Apple Appstore"
           />
         </div>
@@ -50,4 +47,5 @@ const Auth = (props, context) => (
     </div>
   </main>
 );
+
 export default Auth;
