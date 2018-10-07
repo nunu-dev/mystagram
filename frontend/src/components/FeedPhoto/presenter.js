22d6728935b0693674148c94be0a7fb901c53bea
eunwoo <aufcl4858@naver.com>
2018-10-07 23:22:38 +0900

    Making the comments resizeable

--- frontend/src/components/FeedPhoto/presenter.js
+++ frontend/src/components/FeedPhoto/presenter.js
@@ -4,23 +4,24 @@ import styles from './styles.scss';
 import PhotoActions from 'components/PhotoActions';
 import PhotoComments from 'components/PhotoComments';
 import TimeStamp from 'components/TimeStamp';
-import CommentBox from 'components/CommentBox'
+import CommentBox from 'components/CommentBox';
 
 const FeedPhoto = (props, context) => {
   return (
     <div className={styles.feedPhoto}>
-      <header>
+      <header className={styles.header}>
         <img
           src={props.creator.profile_image || require('images/noPhoto.jpg')}
           alt={props.creator.username}
+          className={styles.image}
         />
-        <div>
-          <span>{props.creator.username}</span>
-          <span>{props.location}</span>
+        <div className={styles.headerColumn}>
+          <span className={styles.creator}>{props.creator.username}</span>
+          <span className={styles.location}>{props.location}</span>
         </div>
       </header>
       <img src={props.file} alt={props.caption} />
-      <div>
+      <div className={styles.meta}>
         <PhotoActions number={props.like_count} />
         <PhotoComments
           caption={props.caption}
@@ -28,7 +29,7 @@ const FeedPhoto = (props, context) => {
           comments={props.comments}
         />
         <TimeStamp time={props.natural_time} />
-        <CommentBox  />
+        <CommentBox />
       </div>
     </div>
   );
--- frontend/src/components/PhotoActions/presenter.js
+++ frontend/src/components/PhotoActions/presenter.js
@@ -1,21 +1,21 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Ionicon from "react-ionicons";
-import styles from "./styles.scss";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Ionicon from 'react-ionicons';
+import styles from './styles.scss';
 
 const PhotoActions = (props, context) => (
-  <div>
-    <div>
-      <span>
+  <div className={styles.actions}>
+    <div className={styles.icons}>
+      <span className={styles.icon}>
         <Ionicon icon="ios-heart-outline" fontSize="28px" color="black" />
       </span>
-      <span>
+      <span className={styles.icon}>
         <Ionicon icon="ios-text-outline" fontSize="28px" color="black" />
       </span>
     </div>
-    <span>
-      {props.number}{" "}
-      {props.number === 1 ? context.t("like") : context.t("likes")}
+    <span className={styles.likes}>
+      {props.number}{' '}
+      {props.number === 1 ? context.t('like') : context.t('likes')}
     </span>
   </div>
 );
