8ce778e221b5a1a20b21ed5f74b90f626ef91fa7
eunwoo <aufcl4858@naver.com>
2018-10-14 18:19:57 +0900

    Controlling Comment Component

--- frontend/src/components/CommentBox/container.js
+++ frontend/src/components/CommentBox/container.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CommentBox from './presenter';
 
-const Container = props => <CommentBox {...props} />;
+class Container extends Component {
+  state = {
+    comment: ''
+  };
+  render() {
+    return (
+      <CommentBox
+        {...this.state}
+        handleInputChange={this._handleInputChange}
+        handleKeyPress={this._handleKeyPress}
+      />
+    );
+  }
+  _handleInputChange = event => {
+    const {
+      target: { value }
+    } = event;
+    this.setState({
+      comment: value
+    });
+  };
+  _handleKeyPress = event => {
+    const { key } = event;
+    if (key === 'Enter') {
+      event.preventDefault();
+    }
+    console.log(key);
+  };
+}
 
 export default Container;
