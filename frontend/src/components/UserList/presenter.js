92023a44361ec62f52d08ada77654fc860c3bc95
eunwoo <aufcl4858@naver.com>
2018-10-14 20:59:31 +0900

    Getting and setting the Likes

--- frontend/src/components/UserList/presenter.js
+++ frontend/src/components/UserList/presenter.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './styles.scss';
 import Loading from 'components/Loading';
 import Ionicon from 'react-ionicons';
+
 const UserList = props => (
   <div className={styles.container}>
     <div className={styles.overlay} />
@@ -21,7 +22,7 @@ const LoadingUserList = props =>
   (UserList.propTypes = {
     title: PropTypes.string.isRequired,
     loading: PropTypes.bool.isRequired,
-    users: PropTypes.array,
+    userList: PropTypes.array,
     closeLikes: PropTypes.func.isRequired
   });
 export default UserList;
