a2d9dc11084956ccaded0def3769a5ce0c930418
eunwoo <aufcl4858@naver.com>
2018-10-15 00:17:44 +0900

    Explore

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -7,6 +7,7 @@ const LOGOUT = 'LOGOUT';
 const SET_PHOTO_LIKES = 'SET_PHOTO_LIKES';
 const FOLLOW_USER = 'FOLLOW_USER';
 const UNFOLLOW_USER = 'UNFOLLOW_USER';
+const SET_EXPLORE = 'SET_EXPLORE';
 
 // action creators
 
@@ -41,6 +42,13 @@ function setPhotoLikes(likes) {
   return {
     type: SET_PHOTO_LIKES,
     likes
+  };
+}
+
+function setExplore(userList) {
+  return {
+    type: SET_EXPLORE,
+    userList
   };
 }
 
@@ -180,6 +188,28 @@ function unfollowUser(userId) {
   };
 }
 
+function getExplore() {
+  return (dispatch, getState) => {
+    const {
+      user: { token }
+    } = getState();
+    fetch('/users/explore/', {
+      method: 'GET',
+      headers: {
+        Authorization: `JWT ${token}`,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        if (response.status === 401) {
+          dispatch(logout());
+        }
+        return response.json();
+      })
+      .then(json => dispatch(setExplore(json)));
+  };
+}
+
 // initial state
 
 const initialState = {
@@ -201,6 +231,8 @@ function reducer(state = initialState, action) {
       return applyFollowUser(state, action);
     case UNFOLLOW_USER:
       return applyUnfollowUser(state, action);
+    case SET_EXPLORE:
+      return applySetExplore(state, action);
     default:
       return state;
   }
@@ -260,6 +292,14 @@ function applyUnfollowUser(state, action) {
   return { ...state, userList: updatedUserList };
 }
 
+function applySetExplore(state, action) {
+  const { userList } = action;
+  return {
+    ...state,
+    userList
+  };
+}
+
 // exports
 
 const actionCreators = {
@@ -269,7 +309,8 @@ const actionCreators = {
   logout,
   getPhotoLikes,
   followUser,
-  unfollowUser
+  unfollowUser,
+  getExplore
 };
 
 export { actionCreators };
