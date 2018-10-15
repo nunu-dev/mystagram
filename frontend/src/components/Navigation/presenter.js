baf61bda2c4415e28a60e31ea7d4fcc8785f231e
eunwoo <aufcl4858@naver.com>
2018-10-15 23:28:48 +0900

    Search Component pt. One

--- frontend/src/components/Navigation/presenter.js
+++ frontend/src/components/Navigation/presenter.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Ionicon from "react-ionicons";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import styles from "./styles.scss";
+import React from 'react';
+import Ionicon from 'react-ionicons';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import styles from './styles.scss';
 
 const Navigation = (props, context) => (
   <div className={styles.navigation}>
@@ -10,18 +10,22 @@ const Navigation = (props, context) => (
       <div className={styles.column}>
         <Link to="/">
           <img
-            src={require("images/logo.png")}
+            src={require('images/logo.png')}
             className={styles.logo}
-            alt={context.t("Logo")}
+            alt={context.t('Logo')}
           />
         </Link>
       </div>
       <div className={styles.column}>
-        <input
-          type="text"
-          placeholder={context.t("Search")}
-          className={styles.searchInput}
-        />
+        <form onSubmit={props.onSubmit}>
+          <input
+            type="text"
+            placeholder={context.t('Search')}
+            className={styles.searchInput}
+            value={props.value}
+            onChange={props.onInputChange}
+          />
+        </form>
       </div>
       <div className={styles.column}>
         <div className={styles.navIcon}>
@@ -44,6 +48,12 @@ const Navigation = (props, context) => (
 
 Navigation.contextTypes = {
   t: PropTypes.func.isRequired
+};
+
+Navigation.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired
 };
 
 export default Navigation;
